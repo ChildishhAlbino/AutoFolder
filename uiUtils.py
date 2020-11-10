b5ac975eb199678364762ce425635321855ce2a6
Connor <connorwilliams.home@gmail.com>
2020-11-10 23:49:30 +1100

    FIX: push hotfix.

--- uiUtils.py
+++ uiUtils.py
@@ -2,8 +2,12 @@
 
 def logFiltered(index, item, collection):
     length = len(collection)
+    if(length == 0):
+        return item
     itemNo = index + 1
     chunkSize = int(length / 10)
+    if(chunkSize == 0):
+        return item
     if(index == 0):
         print("0 percent complete")
     elif(itemNo % chunkSize == 0):
